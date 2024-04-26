5563f2c729fe57bc56d52585297c0f0b94c42d76
Liam cariou <cariou.liam@orange.fr>
2024-04-27 00:54:19 +0200

    config env envlocal

--- config/config.php
+++ config/config.php
@@ -12,7 +12,13 @@ function parseEnv($file)
     return $env;
 }
 
-$env = parseEnv('.env');
+function isTestDomain($url)
+{
+    return (strpos($url, 'https://www.beta-test-website.com') !== false);
+}
+
+$envFile = isTestDomain($_SERVER['HTTP_HOST']) ? '.env' : '.env.local';
+$env = parseEnv($envFile);
 
 define('DB_HOST', $env['DB_HOST'] ?? '');
 define('DB_NAME', $env['DB_NAME'] ?? '');
@@ -20,13 +26,14 @@ define('DB_USER', $env['DB_USER'] ?? '');
 define('DB_PASS', $env['DB_PASS'] ?? '');
 define('DB_PORT', $env['DB_PORT'] ?? '');
 define('BASE_URL', $env['BASE_URL'] ?? '');
-
-
 define('ROOT_PATH', realpath(dirname(__FILE__) . '/'));
 
+define('MAIL_HOST', $env['MAIL_HOST'] ?? '');
+define('MAIL_USERNAME', $env['MAIL_USERNAME'] ?? '');
+define('MAIL_PASSWORD', $env['MAIL_PASSWORD'] ?? '');
+define('MAIL_PORT', $env['MAIL_PORT'] ?? '');
+define('MAIL_ENCRYPTION', $env['MAIL_ENCRYPTION'] ?? '');
+define('MAIL_SEND', $env['MAIL_SEND'] ?? '');
 
-
-
-define('NOM_ENTREPRISE', 'Liam Cariou')
-
+define('NOM_ENTREPRISE', 'Liam Cariou');
 ?>
